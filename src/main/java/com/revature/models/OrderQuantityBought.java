8d8627ac17660b4e1e46fe9b3a49306376ed79e9
npn225 <32421292+npn225@users.noreply.github.com>
2022-09-06 16:44:04 -0400

    Update OrderQuantityBought.java
    Fixing merge issues - Ruslan S. & Nnamdi N.

--- src/main/java/com/revature/models/OrderQuantityBought.java
+++ src/main/java/com/revature/models/OrderQuantityBought.java
@@ -1,8 +1,5 @@
 package com.revature.models;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -21,28 +18,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OrderQuantityBought {
 
-=======
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-public class OrderQuantityBought {
-<<<<<<< HEAD
-=======
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
     @EmbeddedId
     OrderQuantityKey id;
 
     @ManyToOne
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     @MapsId("transactionId")
     @JoinColumn(name = "transaction_id")
     private Transaction transactionId;
@@ -54,24 +33,4 @@ public class OrderQuantityBought {
 
     private int quantity;
 
-=======
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
-    @MapsId("orderId")
-    @JoinColumn(name="order_id")
-    private Transaction order_id;
-
-    @ManyToOne
-    @MapsId("productId")
-    @JoinColumn(name="Product_id")
-    private Product product_id;
-
-    private int quantity;
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 }
