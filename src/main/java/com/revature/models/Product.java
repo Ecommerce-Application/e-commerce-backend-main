a26b23d98c6aede820b2e9f8c4940d91ae2651bf
npn225 <32421292+npn225@users.noreply.github.com>
2022-09-06 16:52:25 -0400

    Update Product.java
    Fixing merge issues - Ruslan S. & Nnamdi N.

--- src/main/java/com/revature/models/Product.java
+++ src/main/java/com/revature/models/Product.java
@@ -1,13 +1,9 @@
 package com.revature.models;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-import javax.persistence.*;
-=======
-<<<<<<< HEAD
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -16,34 +12,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-=======
-import javax.persistence.*;
-import java.util.List;
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 
+@Data
 @Entity
-<<<<<<< HEAD
-@Table(name="products")
-@Getter
-@Setter
-=======
 @Table(name = "products")
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "prod_id")
 public class Product {
 
     @Id
-    @Column(unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
-    private int prod_id;
-
-    @Column
-=======
-<<<<<<< HEAD
     private int prodId;
     private int prodQuantity;
     private double prodPrice;
@@ -52,26 +30,5 @@ public class Product {
     private String prodName;
 
     @OneToMany(mappedBy = "productId")
-=======
-    private int id;
-    private int quantity;
-    private double price;
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
-    private String description;
-
-    @Column
-    private String image;
-<<<<<<< HEAD
-
-    @Column
-    private String prod_name;
-
-
-=======
-    private String name;
-
-    @OneToMany(mappedBy = "product_id")
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
     private List<OrderQuantityBought> quantityBoughts;
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 }
