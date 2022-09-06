b052d92c200be56fb9d66a201854e66105293980
npn225 <32421292+npn225@users.noreply.github.com>
2022-09-06 16:54:10 -0400

    Update Wish.java
    Fixing merge issues - Ruslan S. & Nnamdi N.

--- src/main/java/com/revature/models/Wish.java
+++ src/main/java/com/revature/models/Wish.java
@@ -1,13 +1,5 @@
 package com.revature.models;
 
-<<<<<<< HEAD
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Entity
-=======
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,7 +16,6 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "wishes")
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 @NoArgsConstructor
 @AllArgsConstructor
 public class Wish {
@@ -33,20 +24,6 @@ public class Wish {
     WishKey id;
 
     @ManyToOne
-<<<<<<< HEAD
-    @MapsId("user_id")
-    @JoinColumn(name="user_id")
-    private User user_id;
-
-    @ManyToOne
-    @MapsId("productId")
-    @JoinColumn(name="Product_id")
-    private Product product_id;
-
-    @Column(unique = true)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int whish_id;
-=======
     @MapsId("userId")
     @JoinColumn(name = "user_id")
     private User userId;
@@ -61,5 +38,4 @@ public class Wish {
 
     // private String groupId;
     // private String groupName;
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 }
