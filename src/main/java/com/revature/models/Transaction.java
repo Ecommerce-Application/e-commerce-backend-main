89bf12595ec5f4ce0584fb3fced52a8b19e01ef3
npn225 <32421292+npn225@users.noreply.github.com>
2022-09-06 16:52:59 -0400

    Update Transaction.java
    Fixing merge issues - Ruslan S. & Nnamdi N.

--- src/main/java/com/revature/models/Transaction.java
+++ src/main/java/com/revature/models/Transaction.java
@@ -1,8 +1,5 @@
 package com.revature.models;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -12,16 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-=======
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 @Data
 @Entity
 @Table(name = "transactions")
@@ -31,40 +22,11 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int transactionId;
-=======
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
-
-public class Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int orderId;
-
-<<<<<<< HEAD
-=======
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
     private int userId;
     private double total;
     private long datePlaced;
 
-<<<<<<< HEAD
-    @OneToMany(mappedBy = "order_id")
-    private List<OrderQuantityBought> orderQuantityBoughts;
-=======
-<<<<<<< HEAD
     @OneToMany(mappedBy = "transactionId")
     private List<OrderQuantityBought> orderQuantityBoughts;
 
-=======
-    @OneToMany(mappedBy = "order_id")
-    private List<OrderQuantityBought> orderQuantityBoughts;
->>>>>>> c4351952ca92066fdf6b8436ecb0f7a07e59985b
->>>>>>> a4d472441f60ffe298410be441c00293285ad453
 }
