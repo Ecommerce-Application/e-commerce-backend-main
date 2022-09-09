6e7382d77d71a88d6de962d12ca1b1a57d5f70a0
nnamdi <npn225@nyu.edu>
2022-09-09 15:10:23 -0400

    Removed cross-origins all from WishController.java - Nnamdi N.

--- src/main/java/com/revature/controllers/WishController.java
+++ src/main/java/com/revature/controllers/WishController.java
@@ -21,7 +21,8 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:3000",
+        "http://localhost:5000" }, allowedHeaders = "*")
 @RequestMapping("/wish")
 public class WishController {
 
@@ -32,20 +33,18 @@ public class WishController {
 
     @Autowired
     public WishController(WishService wishService, JwtTokenManager tokenManager, UserService userService,
-                          ProductService productService) {
+            ProductService productService) {
         this.wishService = wishService;
         this.tokenManager = tokenManager;
         this.userService = userService;
         this.productService = productService;
     }
 
-
-
     @GetMapping("/get/{user_id}")
     public ResponseEntity<List<Product>> getWishesByUserId(@PathVariable("user_id") int userId) {
         List<Wish> wishes = new ArrayList<Wish>(wishService.getWishesByUserId(userId));
         List<Product> prodList = new ArrayList<Product>();
-        for(int i = 0; i <= wishes.size() - 1; i++) {
+        for (int i = 0; i <= wishes.size() - 1; i++) {
             Product item = wishes.get(i).getProduct();
             prodList.add(item);
         }
@@ -54,27 +53,27 @@ public class WishController {
 
     @PostMapping("/post/{user_id}/{prod_id}")
     public ResponseEntity<Wish> addWish(@PathVariable("user_id") int userId,
-                                        @PathVariable("prod_id") int prodId ) {
-//        try {
-//            wishService.getWishByUserIdAndProductId(userId, prodId);
-//        } catch(WishNotFoundException e) {
-//            return new ResponseEntity<Wish>(HttpStatus.NOT_FOUND);
-//        }
+            @PathVariable("prod_id") int prodId) {
+        // try {
+        // wishService.getWishByUserIdAndProductId(userId, prodId);
+        // } catch(WishNotFoundException e) {
+        // return new ResponseEntity<Wish>(HttpStatus.NOT_FOUND);
+        // }
         System.out.println(userId + "User ID");
         System.out.println(prodId + "Prod ID");
         User user = userService.findByUserId(userId);
         System.out.println(user + "Full user");
-        Product product = productService.findById(prodId).orElseThrow( () ->
-                new ProductNotFoundException("No product with id " + prodId + " found."));
+        Product product = productService.findById(prodId)
+                .orElseThrow(() -> new ProductNotFoundException("No product with id " + prodId + " found."));
         System.out.println(product + "Full product");
         return ResponseEntity.ok(wishService.addWish(user, product));
     }
 
     @DeleteMapping("/delete/{user_id}/{prod_id}")
-    public void deleteWish(@PathVariable("user_id") int userId, @PathVariable("prod_id") int prodId ) {
+    public void deleteWish(@PathVariable("user_id") int userId, @PathVariable("prod_id") int prodId) {
         User user = userService.findByUserId(userId);
-        Product product = productService.findById(prodId).orElseThrow( () ->
-                new ProductNotFoundException("No product with id " + prodId + " found."));
+        Product product = productService.findById(prodId)
+                .orElseThrow(() -> new ProductNotFoundException("No product with id " + prodId + " found."));
         wishService.deleteWish(user, product);
     }
 
